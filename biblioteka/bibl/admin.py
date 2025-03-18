8a3b525f85d642eb097859823d7e638f6c686fdc
kolevas <snezhana.koleva@students.finki.ukim.mk>
2025-03-18 14:50:32 +0100

    Lab 1a complete

--- biblioteka/bibl/admin.py
+++ biblioteka/bibl/admin.py
@@ -3,27 +3,78 @@ from .models import Translator, Genre, Book, Author, BookGenres, BookAuthors, Bo
 
 
 class RatingInline(admin.TabularInline):
+    exclude = ("user",)
     model = Rating
+    readonly_fields = ("user",)
     extra = 2
     def has_change_permission(self, request, obj=None):
         return False
     def has_delete_permission(self, request, obj=None):
-        if obj is None:
+        # if obj is None or request.user != obj.added_by:
+        #     return True
+        # return any(rating.user == request.user for rating in obj.rating_set.all())
+        return True
+    def has_add_permission(self, request, obj=None):
+        if obj is None or request.user != obj.added_by:
             return True
-        return any(rating.user == request.user for rating in obj.rating_set.all())
+        return request.user != obj.added_by
+        # i want to set the user of the rating object to be the user from the request, but rating is inline
 
 
 class BookGenresInline(admin.TabularInline):
     model = BookGenres
     extra = 1
 
+    def has_change_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
 class BookAuthorsInline(admin.TabularInline):
     model = BookAuthors
     extra = 1
 
+    def has_change_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
 class BookTranslationsInline(admin.TabularInline):
     model = BookTranslators
     extra = 1
+    def has_change_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+    def has_delete_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        if obj is None or request.user == obj.added_by:
+            return True
+        return False
 
 class BookAdmin(admin.ModelAdmin):
     exclude = ("added_by",)
@@ -31,10 +82,15 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("available",)
     inlines = [BookGenresInline, BookAuthorsInline, BookTranslationsInline, RatingInline]
 
-    def has_change_permission(self, request, obj=None):
-        if obj is None:
-            return True
-        return obj.added_by == request.user
+    def get_readonly_fields(self, request, obj=None):
+        if obj is None or obj.added_by == request.user:
+            return []
+        return ["title", "issue_date", "added_by", "available", "book_cover", "num_pages",]
+
+    # def has_change_permission(self, request, obj=None):
+    #     if obj is None:
+    #         return True
+    #     return obj.added_by == request.user
 
     def has_delete_permission(self, request, obj=None):
         if obj is None:
@@ -42,8 +98,26 @@ class BookAdmin(admin.ModelAdmin):
         return obj.added_by == request.user
 
     def save_model(self, request, obj, form, change):
-        obj.added_by = request.user
+        if not change:
+            obj.added_by = request.user
+        obj.save()
         return super(BookAdmin, self).save_model(request, obj, form, change)
+
+    def save_formset(self, request, form, formset, change):
+        """Ensure Rating objects get the current user as the 'user' field, and allow deletions."""
+        instances = formset.save(commit=False)  # Get unsaved inline instances
+
+        # Handle newly added instances
+        for instance in instances:
+            if isinstance(instance, Rating) and not instance.pk:
+                instance.user = request.user  # Assign current user
+            instance.save()  # Save each instance
+
+        # Handle deleted instances
+        for obj in formset.deleted_objects:
+            obj.delete()  # Explicitly delete removed objects
+
+        formset.save_m2m()  # Save many-to-many relationships if any
 
 
 admin.site.register(Translator)
